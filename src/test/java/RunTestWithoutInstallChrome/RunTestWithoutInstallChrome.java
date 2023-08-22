464ac254ce9309d0d8808f31df05e86fb0fa4673
Sachin <you@example.com>
2023-08-22 07:55:50 +0000

    Modify Property file

---
+++ src/test/java/RunTestWithoutInstallChrome/RunTestWithoutInstallChrome.java
@@ -0,0 +1,15 @@
+package RunTestWithoutInstallChrome;
+
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class RunTestWithoutInstallChrome {
+	@Test
+	public void CheckChrome() {
+	ChromeOptions options= new ChromeOptions();
+	
+	
+		
+	}
+}
+
